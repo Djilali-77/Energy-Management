dbd00c1ee4df004b88246b0c7b1edde47d109dbc
Djilali-77 <faraoundjilali.fd@gmail.com>
2026-08-13 13:33:03 +0100

    Battery v2

--- src/battery.py
+++ src/battery.py
@@ -1,6 +1,6 @@
 class Battery:
     def __init__(self, soc):
-        
+
         self.capacity = 40
         self.soc = soc
         self.max_charge_power = 5
@@ -11,42 +11,46 @@ class Battery:
 
     def charge(self, solar, demande):
 
-        charge = 0
         surplus = solar - demande
-        stored_energy =  self.soc / 100 * self.capacity
-        remimnig = self.capacity - stored_energy
-        if(solar > demande and self.soc < self.soc_max ):
-            if(surplus < self.max_charge_power):
-                charge =  surplus * self.efficiency
-                if(charge <= remimnig): 
-                    self.soc = stored_energy + charge
-                    self.soc =  100/self.capacity * self.soc 
-            else :
-                charge = self.max_charge_power * self.efficiency
-                if(charge <= remimnig): 
-                    self.soc = stored_energy + charge 
-                    self.soc =  100/self.capacity * self.soc
 
-            
+        if surplus <= 0 or self.soc >= self.soc_max:
+            return 0
+
+        stored_energy = self.soc / 100 * self.capacity
+        remaining = self.capacity * self.soc_max / 100 - stored_energy
+
+        charge_power = min(surplus, self.max_charge_power)
+
+        energy_stored = charge_power * self.efficiency
+
+        energy_stored = min(energy_stored, remaining)
+
+        self.soc = (stored_energy + energy_stored) / self.capacity * 100
+
+        return energy_stored
+
 
     def discharge(self, solar, demande):
-        deficit =  demande - solar
-        stored_energy =  self.soc / 100 * self.capacity
-        minimum_energy = self.capacity  * (self.soc_min/100)
+
+        deficit = demande - solar
+
+        if deficit <= 0 or self.soc <= self.soc_min:
+            return 0
+
+        stored_energy = self.soc / 100 * self.capacity
+        minimum_energy = self.capacity * self.soc_min / 100
+
         available = stored_energy - minimum_energy
 
-        if(solar < demande and self.soc > self.soc_min ):
-            if(deficit < self.max_discharge_power):
-                discharge =  deficit / self.efficiency
-                if(discharge <= available): 
-                    self.soc = stored_energy - discharge
-                    self.soc =  100/self.capacity * self.soc 
-                else :
-                    discharge = self.max_discharge_power / self.efficiency
-                    if discharge <= available :
-                        self.soc = stored_energy - discharge 
-                        self.soc =  100/self.capacity * self.soc
-                    else :
-                        discharge = available
-                        stored_energy -= discharge
-                        self.soc = self.soc_min
+        discharge_power = min(deficit, self.max_discharge_power)
+
+        energy_needed_from_battery = discharge_power / self.efficiency
+
+        if energy_needed_from_battery > available:
+            energy_needed_from_battery = available
+
+        energy_delivered = energy_needed_from_battery * self.efficiency
+
+        self.soc = (stored_energy - energy_needed_from_battery) / self.capacity * 100
+
+        return energy_delivered
