c001392d0da8928d9234489da948626bca75fa4a
Djilali-77 <faraoundjilali.fd@gmail.com>
2026-08-13 13:33:25 +0100

    EV v2

--- src/ev.py
+++ src/ev.py
@@ -3,22 +3,30 @@ class Ev:
 
         self.battery_capacity = 60
         self.soc = soc
-        self.soc_min = 60 
+        self.soc_min = 60
         self.SOC_target = 80
         self.max_charge_power = 7
         self.arrival_hour = arrival_hour
         self.departure_hour = departure_hour
 
-    def charge(self, hour, pv, load):
-        stored_energy =  self.soc / 100 * self.battery_capacity
+    def charge(self, hour, surplus):
 
         if self.arrival_hour <= hour < self.departure_hour:
+
             if self.soc < self.SOC_target:
-                charge = pv - load
-                if charge < self.max_charge_power:
-                    self.soc = stored_energy + charge
-                    self.soc =  100/self.battery_capacity * self.soc 
-                else:
-                    self.soc = stored_energy + self.max_charge_power 
-                    self.soc =  100/self.battery_capacity * self.soc
- 
+
+                stored_energy = self.soc / 100 * self.battery_capacity
+                target_energy = self.SOC_target / 100 * self.battery_capacity
+                remaining_capacity = target_energy - stored_energy
+
+                charge = min(
+                    surplus,
+                    self.max_charge_power,
+                    remaining_capacity
+                )
+
+                self.soc = (stored_energy + charge) / self.battery_capacity * 100
+
+                return charge
+
+        return 0
